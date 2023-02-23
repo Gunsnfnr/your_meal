6549459822169b5d6aa235e45365574ad332ffbe
Gunsnfnr <a.j.p.010203@gmail.com>
2023-02-23 17:40:35 +0300

    add Header & Container

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Container } from "./components/Container/container.jsx"
+import { Header } from "./components/Header/Header.jsx"
+
+export const App = () => {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <nav>
+          <Container className="navigation__container"/>
+        </nav>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
